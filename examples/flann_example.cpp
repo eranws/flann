76b42956ced471c7aa91eb50f23d310426592762
eranws <eranws@gmail.com>
2014-05-26 16:29:58 +0300

    cpp example gets input parameters

--- examples/flann_example.cpp
+++ examples/flann_example.cpp
@@ -11,19 +11,32 @@ int main(int argc, char** argv)
 
 	if (argc < 5)
 	{
-		printf("Usage: %s <filein1> <name1> <filein2> <name2> <fileout> \n", argv[0]);
-		printf("example: %s %s %s %s %s %s\n",  argv[0], "dataset.hdf5","dataset", "dataset.hdf5","query", "result.hdf5");
+		printf("Usage: %s <filein1> <name1> <filein2> <name2> <fileout> <nameout> [n_trees] [n_checks]\n", argv[0]);
+		printf("example: %s %s %s %s %s %s %d %d\n",  argv[0], "dataset.hdf5","dataset", "dataset.hdf5","query", "result.hdf5" "result", 8, 128);
 		exit(1);
 	}
 
-	std::string f1, n1, f2, n2, fo;
+	std::string f1, n1, f2, n2, fo, no;
 	f1 = argv[1];
 	n1 = argv[2];
 	f2 = argv[3];
 	n2 = argv[4];
-
+	
 	fo = argv[5];
+	no = argv[6];
 
+	int n_trees = 8;
+	if (argc > 7)
+	{
+		n_trees = atoi(argv[7]);
+	}
+	
+	int n_checks = 128;
+	if (argc > 8)
+	{
+		n_checks = atoi(argv[8]);
+	}
+	
 
 	Matrix<float> dataset;
     Matrix<float> query;
@@ -39,14 +52,14 @@ int main(int argc, char** argv)
     Matrix<int> indices(new int[query.rows*nn], query.rows, nn);
     Matrix<float> dists(new float[query.rows*nn], query.rows, nn);
 
-    // construct an randomized kd-tree index using 4 kd-trees
-    Index<L2<float> > index(dataset, flann::KDTreeIndexParams(8));
+    // construct an randomized kd-tree index using n_trees kd-trees
+    Index<L2<float> > index(dataset, flann::KDTreeIndexParams(n_trees));
     index.buildIndex();                                                                                               
 
     // do a knn search, using 128 checks
-    index.knnSearch(query, indices, dists, nn, flann::SearchParams(128));
+    index.knnSearch(query, indices, dists, nn, flann::SearchParams(n_checks));
 
-    flann::save_to_file(indices, fo, "result");
+    flann::save_to_file(indices, fo, no);
     flann::save_to_file(dists, fo, "dists");
 
 	Matrix<int> cc(new int[1], 1, 1);
